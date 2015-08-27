5a3c6f08856b7cb5dce9660ba836aabf9e1b5e52
alain cabrera <acabreraglz@hotmail.com>
2015-08-26 20:51:37 -0500

    ui

---
+++ ui.R
@@ -0,0 +1,31 @@
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Box-Muller"),
+  
+  # Sidebar with a slider input for the number of bins
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("bins",
+                   "N?mero de simulaciones:",
+                   value = 300),
+    
+      downloadButton('downloadData', 'Download')
+      
+      
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Plot", plotOutput("distPlot")), 
+        tabPanel("Table", tableOutput("table")),
+        tabPanel("Summary", verbatimTextOutput("summary"))
+        
+      )      
+    )
+  )
+))
